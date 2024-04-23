af47927e4710bab7bd72c3831e3c78c0442f3115
Danny Edwards <danny_edwards09@icloud.com>
2024-04-23 10:55:20 +0800

    square accelerates towards the ground and reaches terminal velocity if given enough time. Currently falls when holding spacebar for testing. is clipping to the floor for some reason.

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,78 @@
 #include <fstream>
 #include <SDL2/SDL.h>
 
-class Point {
+#define GRAVITY 9.81
+#define AIR_DENSITY 1.225
+
+enum ObjectState {
+  InAir,
+  OnFloor
+};
+
+enum ObjectShape {
+  SquareS,
+  CircleS
+};
+
+class Square {
   public:
-    Point(double x, double y) : x(x), y(y) {}
+    Square(double x, double y, double size) : x(x), y(y), size(size) {}
     double x;
     double y;
+    double size;            //m^2
+    double velocity = 0;    //m/s
+    double mass = 100;        //kg
+    double dt = 0.1;        //s
+    ObjectState state;
+    ObjectShape shape = ObjectShape::SquareS;
+  
+    void draw(SDL_Renderer* renderer) {
+      SDL_Rect rect;
+      rect.x = x;
+      rect.y = y;
+      std::cout << "Y: " << y << "\n";
+      rect.w = size;
+      rect.h = size;
+      SDL_RenderFillRect(renderer, &rect);
+    }
+
+    double dragCoefficient(ObjectShape shape) {
+      switch (shape) {
+        case ObjectShape::SquareS:
+          return 1.05;
+        case ObjectShape::CircleS:
+          return 0.47;
+        default:
+          std::cout << "Invalid shape\n";
+          return 1.05;
+      }
+    }
+    
+    double accelerationY(ObjectShape shape) {
+      double drag = 0.5 * AIR_DENSITY * dragCoefficient(shape) * pow(velocity, 2) * size;
+      double forceY = mass * GRAVITY;
+      double accelerationY = forceY / mass;
+
+      accelerationY = (accelerationY - drag / mass) * dt;      //divide by mass as drag is a force and F = ma
+
+      return accelerationY;
+    }
+
+    void update(int floorY) {
+      velocity += accelerationY(shape) * dt;
+      std::cout << "Velocity: " << velocity << std::endl;
+
+      if (y >= floorY) {
+        y = floorY;
+        velocity = 0;
+        y = floorY - size;
+        state = ObjectState::OnFloor;
+      } else {
+        state = ObjectState::InAir;
+      }
+
+      y += velocity;
+    }
 };
 
 int initialiseSDL() {
@@ -66,6 +133,11 @@ int main() {
 
   SDL_Renderer* renderer = createRenderer(window);
 
+  int window_width, window_height;
+
+  bool checkStep = true;
+
+  Square object = Square(100, 100, 50);
   // Event loop
   SDL_Event event;
  bool quit = false;
@@ -74,12 +146,33 @@ int main() {
           if (event.type == SDL_QUIT) {
               quit = true;
           }
+          if (event.type == SDL_KEYDOWN) {
+            if (event.key.keysym.sym == SDLK_SPACE) {
+              checkStep = true;
+            }
+          }
       }
-      // Rendering code moved outside of the event loop
-      SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-      SDL_RenderClear(renderer); // Clear the renderer to a solid color
-      SDL_RenderDrawPoint(renderer, 100, 100);
-      SDL_RenderPresent(renderer);
+
+      if (checkStep) {
+        std::cout << "checkStep" << checkStep << std::endl;
+
+        SDL_GetRendererOutputSize(renderer, &window_width, &window_height);
+        //clear screen
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer); // Clear the renderer to a solid color
+
+        //draw floor        
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        int floorY = window_height - 20;
+        SDL_RenderFillRect(renderer, new SDL_Rect{0, floorY, window_width, 10});
+
+        SDL_SetRenderDrawColor(renderer, 255, 20, 20, 255);
+        object.update(floorY);
+        object.draw(renderer);
+        SDL_RenderPresent(renderer);
+
+        checkStep = false;
+      }
  }
   
   
